c2a1d480869fb8c9f26bae5e2788affea2ead14b
Pyotr Ermishkin <quasiyoke@gmail.com>
2015-05-07 14:50:21 +0300

    Closed #7

--- src/utils/VFSImpl.java
+++ src/utils/VFSImpl.java
@@ -15,35 +15,13 @@ public class VFSImpl implements VFS {
         this.root = root;
     }
 
-    public boolean isDirectory(String path) {
-        return new File(root + path).isDirectory();
-    }
-
     public Iterator<String> getIterator(String startDir) {
         return new FileIterator(startDir);
     }
 
     @Override
-    public String getAbsolutePath(String file) {
-        return new File(root + file).getAbsolutePath();
-    }
-
-    @Override
-    public boolean isExist(String path) {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public byte[] getBytes(String file) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public String getUFT8Text(String file) {
-        // TODO Auto-generated method stub
-        return null;
+    public String getName(String path) {
+        return new File(path).getName();
     }
 
     private class FileIterator implements Iterator<String> {
